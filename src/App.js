219a5bf3378c4e692caf1889ea0e077fc5563861
Aaron Lu <aaronnoh0409@gmail.com>
2022-03-05 11:21:19 -0500

    nav body footer added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 
 // Components
-import Topbar from './components/Topbar/Topbar';
-
+import Topbar from './components/Topbar';
+import Footer from './components/Footer';
 
 // Pages
 import Main from './pages/Main'
@@ -20,7 +20,7 @@ function App() {
           <Route path="/comments" element={<Comments />} />
         </Routes>
       </BrowserRouter>
-
+      <Footer />
     </div>
   );
 }
