07ce820b90ba59fdc90a29881d25a09abc3a18f2
Aaron Lu <aaronnoh0409@gmail.com>
2022-03-05 13:43:51 -0500

    this is interesting, need to import bootstrap once more at the app level to modify the bootstrap elements

--- src/components/Searchbar/index.js
+++ src/components/Searchbar/index.js
@@ -1,7 +1,7 @@
 import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
-import 'style.css'
+import './style.css'
 
 function Searchbar() {
     return (
