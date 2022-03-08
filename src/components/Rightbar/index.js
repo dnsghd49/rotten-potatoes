fa6b7522bf9095d5b5db2c4215bada3b6e8bcb67
Aaron Lu <aaronnoh0409@gmail.com>
2022-03-07 20:43:55 -0500

    some what functional dropdown menu added, need to work on the media query

--- src/components/Rightbar/index.js
+++ src/components/Rightbar/index.js
@@ -10,11 +10,48 @@ function Rightbar() {
                 <Nav.Link href="#">SIGN UP</Nav.Link>
                 <Nav.Link href="#">LOG IN</Nav.Link>
             </Nav>
+
             <Nav className="justify-content-end">
-                <Nav.Link href="/movies">Movies</Nav.Link>
+                <div className='dropdown'>
+                    <Nav.Link href="/movies">Movies</Nav.Link>
+                    <div className='dropdown-content'>
+                        <div className='dp-flex'>
+                            <div className='dropdown-items'>
+                                <p>col</p>
+                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
+                                <Nav.Link href="/podcast">RT podcast</Nav.Link>
+                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
+                                <Nav.Link href="/podcast">RT podcast</Nav.Link>
+                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
+                            </div>
+                            <div className='dropdown-items'>
+                                <p>col</p>
+                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
+                                <Nav.Link href="/podcast">RT podcast</Nav.Link>
+                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
+                                <Nav.Link href="/podcast">RT podcast</Nav.Link>
+                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
+                                <Nav.Link href="/podcast">RT podcast</Nav.Link>
+                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
+                            </div>
+                            <div className='dropdown-items'>
+                                <p>col</p>
+                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
+                                <Nav.Link href="/podcast">RT podcast</Nav.Link>
+                            </div>
+                            <div className='dropdown-imgs'>
+                                <p>CERTIFIED FRESH PICKS</p>
+                                <div className='movie-poster'>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <Nav.Link href="/tv-shows">TV shows</Nav.Link>
                 <Nav.Link href="/podcast">RT podcast</Nav.Link>
-                <Nav.Link href="/new">New</Nav.Link>
+
+                <Nav.Link href="/new">News</Nav.Link>
                 <Nav.Link href="/showtimes">Showtimes</Nav.Link>
             </Nav>
         </div>
