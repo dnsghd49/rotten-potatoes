f9bc9b956085eb27cb8b6a77b8f813282582ae56
Aaron Lu <aaronnoh0409@gmail.com>
2022-03-07 21:35:14 -0500

    breaking down rightbar with components for more visibility

--- src/components/Rightbar/index.js
+++ src/components/Rightbar/index.js
@@ -1,6 +1,11 @@
 import Nav from 'react-bootstrap/Nav'
 import './style.css'
 
+// components
+import MoviesDropdown from '../MoviesDropdown'
+import TvshowsDropdown from '../TvshowsDropdown'
+import NewsDropdown from '../NewsDropdown'
+
 // posters 
 // import posterOne from ''
 // import posterTwo from ''
@@ -17,49 +22,17 @@ function Rightbar() {
             </Nav>
 
             <Nav className="justify-content-end">
-                <div className='dropdown'>
-                    <Nav.Link href="/movies">Movies</Nav.Link>
-                    <div className='dropdown-content'>
-                        <div className='dp-flex'>
-                            <div className='dropdown-items'>
-                                <p>MOVIES IN THEATERS</p>
-                                <Nav.Link href="/tv-shows">Opening This Week</Nav.Link>
-                                <Nav.Link href="/podcast">Top Box Office</Nav.Link>
-                                <Nav.Link href="/tv-shows">Coming Soon to Theaters</Nav.Link>
-                                <Nav.Link href="/podcast">Weekend Earnings</Nav.Link>
-                                <Nav.Link href="/tv-shows">Certified Fresh Movies</Nav.Link>
-                            </div>
-                            <div className='dropdown-items'>
-                                <p>ON DVD & STREAMING</p>
-                                <Nav.Link href="/tv-shows">VUDU</Nav.Link>
-                                <Nav.Link href="/podcast">Netflix Streaming</Nav.Link>
-                                <Nav.Link href="/tv-shows">iTunes</Nav.Link>
-                                <Nav.Link href="/podcast">Amazon and Amazon Prime</Nav.Link>
-                                <Nav.Link href="/tv-shows">Top DVD & Streaming</Nav.Link>
-                                <Nav.Link href="/podcast">Certified Fresh Movies</Nav.Link>
-                                <Nav.Link href="/tv-shows">Browse All</Nav.Link>
-                            </div>
-                            <div className='dropdown-items'>
-                                <p>MORE</p>
-                                <Nav.Link href="/tv-shows">Top Movies</Nav.Link>
-                                <Nav.Link href="/podcast">Trailers</Nav.Link>
-                            </div>
-                            <div className='dropdown-imgs'>
-                                <p>CERTIFIED FRESH PICKS</p>
-                                <div className='movie-poster'>
-                                    <img src={{}} alt='movie poster'></img>
-                                    <img src={{}} alt='movie poster'></img>
-                                    <img src={{}} alt='movie poster'></img>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {/* this section is for movies dropdown */}
+                <MoviesDropdown />
 
-                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
+                {/* this section is for TV shows dropdown */}
+                <TvshowsDropdown />
+
                 <Nav.Link href="/podcast">RT podcast</Nav.Link>
 
-                <Nav.Link href="/new">News</Nav.Link>
+                {/* this section is for TV shows dropdown */}
+                <NewsDropdown />
+                
                 <Nav.Link href="/showtimes">Showtimes</Nav.Link>
             </Nav>
         </div>
