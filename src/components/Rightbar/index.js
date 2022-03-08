cc25a462a485f447b32d0d10fd250e4868e84ec0
Aaron Lu <aaronnoh0409@gmail.com>
2022-03-07 20:59:19 -0500

    cleaned up some code, added blank img tags

--- src/components/Rightbar/index.js
+++ src/components/Rightbar/index.js
@@ -1,6 +1,11 @@
 import Nav from 'react-bootstrap/Nav'
 import './style.css'
 
+// posters 
+// import posterOne from ''
+// import posterTwo from ''
+// import posterThree from ''
+
 function Rightbar() {
     return (
         <div className='rightbar-container'>
@@ -17,31 +22,34 @@ function Rightbar() {
                     <div className='dropdown-content'>
                         <div className='dp-flex'>
                             <div className='dropdown-items'>
-                                <p>col</p>
-                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
-                                <Nav.Link href="/podcast">RT podcast</Nav.Link>
-                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
-                                <Nav.Link href="/podcast">RT podcast</Nav.Link>
-                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
+                                <p>MOVIES IN THEATERS</p>
+                                <Nav.Link href="/tv-shows">Opening This Week</Nav.Link>
+                                <Nav.Link href="/podcast">Top Box Office</Nav.Link>
+                                <Nav.Link href="/tv-shows">Coming Soon to Theaters</Nav.Link>
+                                <Nav.Link href="/podcast">Weekend Earnings</Nav.Link>
+                                <Nav.Link href="/tv-shows">Certified Fresh Movies</Nav.Link>
                             </div>
                             <div className='dropdown-items'>
-                                <p>col</p>
-                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
-                                <Nav.Link href="/podcast">RT podcast</Nav.Link>
-                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
-                                <Nav.Link href="/podcast">RT podcast</Nav.Link>
-                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
-                                <Nav.Link href="/podcast">RT podcast</Nav.Link>
-                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
+                                <p>ON DVD & STREAMING</p>
+                                <Nav.Link href="/tv-shows">VUDU</Nav.Link>
+                                <Nav.Link href="/podcast">Netflix Streaming</Nav.Link>
+                                <Nav.Link href="/tv-shows">iTunes</Nav.Link>
+                                <Nav.Link href="/podcast">Amazon and Amazon Prime</Nav.Link>
+                                <Nav.Link href="/tv-shows">Top DVD & Streaming</Nav.Link>
+                                <Nav.Link href="/podcast">Certified Fresh Movies</Nav.Link>
+                                <Nav.Link href="/tv-shows">Browse All</Nav.Link>
                             </div>
                             <div className='dropdown-items'>
-                                <p>col</p>
-                                <Nav.Link href="/tv-shows">TV shows</Nav.Link>
-                                <Nav.Link href="/podcast">RT podcast</Nav.Link>
+                                <p>MORE</p>
+                                <Nav.Link href="/tv-shows">Top Movies</Nav.Link>
+                                <Nav.Link href="/podcast">Trailers</Nav.Link>
                             </div>
                             <div className='dropdown-imgs'>
                                 <p>CERTIFIED FRESH PICKS</p>
                                 <div className='movie-poster'>
+                                    <img src={{}} alt='movie poster'></img>
+                                    <img src={{}} alt='movie poster'></img>
+                                    <img src={{}} alt='movie poster'></img>
                                 </div>
                             </div>
                         </div>
