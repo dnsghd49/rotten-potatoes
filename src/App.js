8707907f8ba4ed0032937cd791b910af462ef9a0
Aaron Lu <aaronnoh0409@gmail.com>
2022-03-09 18:55:38 -0500

    pushing the basic form for backend

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Footer from "./components/Footer";
 // Pages
 import Main from "./pages/Main";
 import Comments from "./pages/Comments";
+import Signup from "./components/Signup";
 
 function App() {
   return (
@@ -19,7 +20,7 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Main />} />
             <Route path="/comments" element={<Comments />} />
-            <Route path="#" element={<Main />} />
+            <Route path="/sign-up" element={<Signup />} />
             <Route path="#" element={<Main />} />
           </Routes>
         </BrowserRouter>
--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,8 +1,36 @@
 import './style.css'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
-function Signup() {
+// name password email
+function Signup(props) {
     return (
-        <div></div>
+        // <div className='popup'>
+        //     <div className='popup-inner'>
+        //         <button className='close-btn' onClick={props.setTrigger(false)}>X</button>
+        //         {props.children}
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+        //     </div>
+        // </div>
     )
 }
 
