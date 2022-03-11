56b1a98950fcc5e074bcffd1ecd3a042a895d9b9
Aaron Lu <aaronnoh0409@gmail.com>
2022-03-11 15:53:40 -0500

    form design modified

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -14,13 +14,13 @@ function Login(props) {
           onClick={() => props.setTrigger(false)}
           icon={faXmark}
         />
-        <Form Form onSubmit={() => props.setTrigger(false)}>
+        <Form className="login-form" onSubmit={() => props.setTrigger(false)}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
+            <Form.Label className="login-label">Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
+            <Form.Label className="login-label">Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
           <Button variant="primary" type="submit">
