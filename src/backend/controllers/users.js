319ab5e9ac622048fcf70a5a83541b94d63eafcb
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2022-03-11 18:27:28 -0500

    updated index in controllers for users queries

--- src/backend/controllers/users.js
+++ src/backend/controllers/users.js
@@ -17,45 +17,6 @@ app.use(function (req, res, next) {
   next();
 });
 
-const getMerchants = () => {
-    return new Promise(function(resolve, reject) {
-      pool.query('SELECT * FROM merchants ORDER BY id ASC', (error, results) => {
-        if (error) {
-          reject(error)
-        }
-        resolve(results.rows);
-      })
-    }) 
-  }
-  const createMerchant = (body) => {
-    return new Promise(function(resolve, reject) {
-      const { name, email } = body
-      pool.query('INSERT INTO merchants (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
-        if (error) {
-          reject(error)
-        }
-        resolve(`A new merchant has been added added: ${results.rows[0]}`)
-      })
-    })
-  }
-  const deleteMerchant = () => {
-    return new Promise(function(resolve, reject) {
-      const id = parseInt(request.params.id)
-      pool.query('DELETE FROM merchants WHERE id = $1', [id], (error, results) => {
-        if (error) {
-          reject(error)
-        }
-        resolve(`Merchant deleted with ID: ${id}`)
-      })
-    })
-  }
-  
-  module.exports = {
-    getMerchants,
-    createMerchant,
-    deleteMerchant,
-  }
-
 app.get('/', (req, res) => {
   user_model.getUsers()
   .then(response => {
