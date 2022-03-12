ce2d65e34073ad4dfa3e35308b0e7da7f5dc077b
Ayron's Macbook <ayayron@Ayrons-MacBook-Pro.local>
2022-03-12 01:04:36 -0500

    fixing spelling mistake

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,7 @@
 function Dashboard() {
     return (
         <div>
-            <h2>welcome to your ROTTEN POTATOS!!!!</h2>
+            <h2>welcome to ROTTEN POTATOES!!!!</h2>
             <h4>at rotten potatos you can say whatever you like to a movie</h4>
         </div>
     )
