de7da28c1a32ae67eb8bf53e80b2a06d6cf86a49
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2022-03-14 00:27:47 -0400

    ignore router routes page

--- src/Routes/user_routes.js
+++ src/Routes/user_routes.js
@@ -1,17 +1,17 @@
-module.exports = app => {
-    const users = require("./controllers/user_controller.js");
-    const router = require("express").Router();
-    // Create a new User
-    router.post("/", users.create);
-    // Retrieve all Users
-    router.get("/", users.findAll);
-    // Retrieve a single User with id
-    router.get("/:id", users.findOne);
-    // Update a User with id
-    router.put("/:id", users.update);
-    // Delete a User with id
-    router.delete("/:id", users.delete);
-    // Delete all Users
-    router.delete("/", users.deleteAll);
-    app.use('/api/users', router);
-  };
+// module.exports = app => {
+//     const users = require("./controllers/user_controller.js");
+//     const router = require("express").Router();
+//     // Create a new User
+//     router.post("/", users.create);
+//     // Retrieve all Users
+//     router.get("/", users.findAll);
+//     // Retrieve a single User with id
+//     router.get("/:id", users.findOne);
+//     // Update a User with id
+//     router.put("/:id", users.update);
+//     // Delete a User with id
+//     router.delete("/:id", users.delete);
+//     // Delete all Users
+//     router.delete("/", users.deleteAll);
+//     app.use('/api/users', router);
+//   };
