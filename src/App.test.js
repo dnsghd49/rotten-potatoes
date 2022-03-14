a7c055bec0c3f01d008b3a3cdab8f605b505423e
Aaron Lu <aaronnoh0409@gmail.com>
2022-03-14 18:51:11 -0400

    installed 2 dependencies for testing navbar clicks and testing imgs, but they don't work :), added just one test to check if the dev page is loading

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App";
+// import { mount } from 'enzyme';
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import Topbar from "./components/Topbar";
+import App from "./App"
+import DevTeam from "./pages/DevTeam";
+
+test('app renders logo', () => {
+  render(<DevTeam />)
+  const navTextElement = screen.getByText(/FRONT-END DEVS/i)
+  expect(navTextElement).toBeInTheDocument()
+})
