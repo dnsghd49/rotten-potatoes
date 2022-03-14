7bff498c4c9e343e84e5cffe82724312928b639e
Aaron Lu <aaronnoh0409@gmail.com>
2022-03-14 18:51:47 -0400

    installed 2 dependencies for testing navbar clicks and testing imgs, but they don't work :), added just one test to check if the dev page is loading

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import Topbar from "./components/Topbar";
 import App from "./App"
 import DevTeam from "./pages/DevTeam";
 
-test('app renders logo', () => {
+test('check if the developers page is loading the text', () => {
   render(<DevTeam />)
   const navTextElement = screen.getByText(/FRONT-END DEVS/i)
   expect(navTextElement).toBeInTheDocument()
