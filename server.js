b2f5b916d6e78da98c20ebee2f577809765f9062
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2022-03-15 16:21:24 -0400

    $

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const sequelize = new Sequelize(process.env.PG_URI,
 
 try {
     sequelize.authenticate() 
-    // console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
+    // console.log(`Connected with Sequelize at ${process.env.PG_URI}`)/
 } catch(err) {
     console.log(`Unable to connect to PG: ${err}`) 
 }
