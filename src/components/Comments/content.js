83ea772d00dee93671be05edf2e7487e107b35e2
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2022-03-14 21:09:54 -0400

    re-added files to comments/content page

--- src/components/Comments/content.js
+++ src/components/Comments/content.js
@@ -34,11 +34,19 @@ const ContentComments = () => {
             <li>{mostPopular.Error}</li>
         );
 
-    const handleFormSubmit = async () => {
-        const response = await fetch("/ratings", {
-            // need to update this area of code to POST what is on the form/ 
-
-        })
+    const handleFormSubmit = async e => {
+        // e.preventDefault();
+            // try {
+            //   const body = { user_id, movie_id, rating, comment };
+            //   const response = await fetch("http://localhost:5000/todos", {
+            //     method: "POST",
+            //     headers: { "Content-Type": "application/json" },
+            //     body: JSON.stringify(body)
+            //   });
+            //   window.location = "/";
+            // } catch (err) {
+            //   console.error(err.message);
+            // };
 
     }
     // STARS RATING
